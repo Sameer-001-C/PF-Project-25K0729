fa9786a545808b39bbfb4490a4d26453f4c24e76
Shaikh Muhammad Sameer <K250729@nu.edu.pk>
2025-11-17 23:03:03 +0500

    Update 02Creating_Directory_With_Input.c

--- Partial_Codes/02Creating_Directory_With_Input.c
+++ Partial_Codes/02Creating_Directory_With_Input.c
@@ -1,4 +1,4 @@
-//take user's name (student ID, eg 25K0729) as an input and create a folder with that name in the directory: D:/PF_Project/Server/Chats/
+//take user's name (student ID, eg 25K0729) as an input and create a folder with that name in the directory: D:/PF_Project/Client/Chats/
 //This directory would store the chats of the client 25K0729
 
 #include <direct.h>
@@ -14,7 +14,7 @@ int main() {
 	
 	scanf("%c", &buffer);
 	
-	char location[100] = "D:/PF_Project/Server/Chats/";
+	char location[100] = "D:/PF_Project/Client/Chats/";
 	strcat(location, Client_A_UserName);
     int status = _mkdir(location);
 
