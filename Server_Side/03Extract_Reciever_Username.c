23e6a09b2d25aef6fec80a2ede8b7388e4207b4e
Shaikh Muhammad Sameer <K250729@nu.edu.pk>
2025-11-19 13:38:51 +0500

    Update 03Extract_Reciever_Username.c

--- Server_Side/03Extract_Reciever_Username.c
+++ Server_Side/03Extract_Reciever_Username.c
@@ -1,14 +1,14 @@
 //message format is this:
 // <Server_Code><Sender_Username><Reciever_Username><message>
 
-void Extract_Reciever_Username(char *message, char *Reciever_Username_Out)
+void Extract_Reciever_Username(char *buffer, char *Reciever_Username_Out)
 {
     // Copy 7 characters starting from message[8]
     for (int i = 0; i < 7; i++) {
-        Reciever_Username_Out[i] = message[i + 8];
+        Reciever_Username_Out[i] = buffer[i + 8];
     }
 
     // Add null terminator at the end
     Reciever_Username_Out[7] = '\0';
 }
-// Use it like this: Extract_Reciever_Username(message, Reciever_Username);   ---> automatically stores in Reciever_Username
+// Use it like this: Extract_Reciever_Username(buffer, Reciever_Username);   ---> automatically stores in Reciever_Username
