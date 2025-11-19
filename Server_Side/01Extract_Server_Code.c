9b214cf30525b6c600fd9b4be7976ed24515e158
Shaikh Muhammad Sameer <K250729@nu.edu.pk>
2025-11-19 13:37:38 +0500

    Update 01Extract_Server_Code.c

--- Server_Side/01Extract_Server_Code.c
+++ Server_Side/01Extract_Server_Code.c
@@ -1,8 +1,7 @@
 // This function will extract the server code for further processing and decision taking
 
-char Extract_Server_Code(char *message){
-    char Server_Code = message[0];
+char Extract_Server_Code(char *buffer){
+    char Server_Code = buffer[0];
     return Server_Code;
 }
-
-// Use it like this: Server_Code = Extract_Server_Code(message)
+// Use it like this: Server_Code = Extract_Server_Code(buffer)
